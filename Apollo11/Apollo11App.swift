f9ae39ba91d7ba92c7cae1ed4a95c32c9b50d9ea
KQAR <31615748+KQAR@users.noreply.github.com>
2023-04-28 14:58:49 +0800

    first commit

---
+++ Apollo11/Apollo11App.swift
@@ -0,0 +1,17 @@
+//
+//  Apollo11App.swift
+//  Apollo11
+//
+//  Created by Jarvis on 2023/4/28.
+//
+
+import SwiftUI
+
+@main
+struct Apollo11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apollo11/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Apollo11
+//
+//  Created by Jarvis on 2023/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
